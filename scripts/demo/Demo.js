578a0f216a7247821ad47d6c228fdc5f4a07e1db
Koen Hendriks <info@koenhendriks.com>
2016-03-13 05:12:21 +0100

    Added missing resources

---
+++ scripts/demo/Demo.js
@@ -0,0 +1,79 @@
+
+var Demo = {
+
+    /**
+     * Container to render the demo in
+     */
+    container : document.createElement( 'div' ),
+
+    /**
+     * Initializing of the demo
+     */
+    init : function(){
+        /**
+         * Set the function to listen to resize changes
+         */
+        window.addEventListener( 'resize', Demo.onWindowResize, false );
+
+        /**
+         * Run the initializing of the various objects that
+         * we use in the Demo
+         */
+        Create.init();
+        Scene.init();
+        AmbientLight.init();
+        Light.init();
+        Plane.init();
+        Camera.init();
+        Render.init();
+
+        /**
+         * Add the container to the body and render
+         * the demo inside the container
+         */
+        document.body.appendChild( Demo.container );
+        Demo.container.appendChild(Render.instance.domElement);
+
+        /**
+         * Run the animation
+         */
+        Demo.animate();
+    },
+
+    /**
+     * Get a random value between 2 integers
+     *
+     * @param min
+     * @param max
+     * @returns {number}
+     */
+    randomIntFromInterval : function (min,max){
+        return Math.floor(Math.random()*(max-min+1)+min);
+    },
+
+    /**
+     * Called when browser window size is changed
+     * Necessary for good camera handling and rendering
+     */
+    onWindowResize : function(){
+        Camera.instance.updateProjectionMatrix();
+        Render.instance.setSize( window.innerWidth, window.innerHeight );
+    },
+
+    /**
+     * Called every frame, animates the Demo
+     */
+    animate : function() {
+        var r = Date.now() * 0.0005;
+        Camera.instance.position.x = 70 * Math.sin( r );
+        Camera.instance.position.y = 70 * Math.cos( -r );
+        Camera.instance.rotateX(Math.sin( r )*0.02);
+        Camera.instance.rotateY(Math.sin( -r )*0.02);
+
+        requestAnimationFrame( Demo.animate );
+        Render.run();
+    }
+};
+
+
+Demo.init();
